9af6a7ff4bb974e87e0f8d8fa2237646f9606c72
Elias Porcio <elias.porcio@hm.edu>
2021-07-29 00:37:38 +0200

    Added READMEClassifier model and download script

---
+++ download_READMEClassifier.sh
@@ -0,0 +1,15 @@
+#READMEClassifier cannot be uploaded to the repository due to a missing license
+#It may be necessary to adjust some imports and remove main-guards from the scripts
+git clone https://github.com/gprana/READMEClassifier.git
+
+mkdir READMEClassifier/log/
+mkdir READMEClassifier/temp/
+mkdir READMEClassifier/temp/abstracted_markdown/
+mkdir READMEClassifier/temp/abstracted_html/
+mkdir READMEClassifier/temp/target_abstracted_markdown/
+mkdir READMEClassifier/input/
+mkdir READMEClassifier/input/clf_target_readmes/
+mkdir READMEClassifier/output/
+
+mv READMEClassifier_model READMEClassifier/model/
+
